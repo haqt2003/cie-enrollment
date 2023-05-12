6ade718f13a1fd269bd1de27b6d60558bc74b0e7
idist <minhtoan96.it@gmail.com>
2023-05-12 10:05:29 +0700

    Fix bug JS

--- assets/script.js
+++ assets/script.js
@@ -1,82 +1,75 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
+var inputItems = $$('.container__form-input-block');
 
 isValid = true;
 
-// // 1 khối chương trình được tạo sẵn
-// const program = `<div class="container__form-input">
-// <div class="container__form-input-block">
-//     <label for="school" class="container__form-input-block-label"><span>*</span>CÁC TRƯỜNG XÉT TUYỂN</label>
-//     <span class="container__form-input-block-label-main">Đại học La Trobe, Úc</span>
-//     <img src="./assets/font/svg/Component 115 – 1.png" alt="">
-//     <span class="container__form-input-block-requireMessage">Trường bắt buộc</span>
-//     <ul class="container__form-input-block-list">
-//         <li class="container__form-input-block-list-item">Đại học La Trobe, Úc</li>
-//         <li class="container__form-input-block-list-item">Đại học Canberra, Úc</li>
-//         <li class="container__form-input-block-list-item">Đại học Bellevue, Mỹ</li>
-//         <li class="container__form-input-block-list-item">Đại học Huddersfield, Vương Quốc Anh</li>
-//         <li class="container__form-input-block-list-item">Đại học Middlesex, Vương Quốc Anh</li>
-//     </ul>
-//     <span class="container__form-input-block-del">Xóa</span>
-// </div>
-// <div class="container__form-input-block">
-//     <label for="school" class="container__form-input-block-label"><span>*</span>NGÀNH XÉT TUYỂN</label>
-//     <span class="container__form-input-block-label-main">Chương trình cử nhân Đa phương tiện 2+2 với Đại học Huddersfield, Vương Quốc Anh</span>
-//     <img src="./assets/font/svg/Component 115 – 1.png" alt="">
-//     <span class="container__form-input-block-requireMessage">Trường bắt buộc</span>
-//     <ul class="container__form-input-block-list">
-//         <li class="container__form-input-block-list-item">Chương trình cử nhân Đa phương tiện 2+2 với Đại học Huddersfield, Vương Quốc Anh</li>
-//         <li class="container__form-input-block-list-item">Chương trình cử nhân Công nghệ thông tin 2+2 với Đại học Huddersfield, Vương Quốc Anh</li>
-//         <li class="container__form-input-block-list-item">Chương trình cử nhân An toàn thông tin 2+2 với Đại học Huddersfield, Vương Quốc Anh</li>
-//         <li class="container__form-input-block-list-item">Chương trình cử nhân Công nghệ kỹ thuật điện, điện tử 2+2 với Đại học Huddersfield, Vương Quốc Anh</li>
-//     </ul>
-//     <span class="container__form-input-block-del-childBlock">Xóa</span>
-// </div>
-// <div class="container__form-input-block-invisible">
+// 1 khối chương trình được tạo sẵn
+const program = document.createRange().createContextualFragment(`<div class="container__form-input">
+<div class="container__form-input-block">
+    <label for="school" class="container__form-input-block-label"><span>*</span>CÁC TRƯỜNG XÉT TUYỂN</label>
+    <span class="container__form-input-block-label-main">Đại học La Trobe, Úc</span>
+    <img src="./assets/font/svg/Component 115 – 1.png" alt="">
+    <span class="container__form-input-block-requireMessage">Trường bắt buộc</span>
+    <ul class="container__form-input-block-list">
+        <li class="container__form-input-block-list-item">Đại học La Trobe, Úc</li>
+        <li class="container__form-input-block-list-item">Đại học Canberra, Úc</li>
+        <li class="container__form-input-block-list-item">Đại học Bellevue, Mỹ</li>
+        <li class="container__form-input-block-list-item">Đại học Huddersfield, Vương Quốc Anh</li>
+        <li class="container__form-input-block-list-item">Đại học Middlesex, Vương Quốc Anh</li>
+    </ul>
+    <span class="container__form-input-block-del">Xóa</span>
+</div>
+<div class="container__form-input-block">
+    <label for="school" class="container__form-input-block-label"><span>*</span>NGÀNH XÉT TUYỂN</label>
+    <span class="container__form-input-block-label-main">Chương trình cử nhân Đa phương tiện 2+2 với Đại học Huddersfield, Vương Quốc Anh</span>
+    <img src="./assets/font/svg/Component 115 – 1.png" alt="">
+    <span class="container__form-input-block-requireMessage">Trường bắt buộc</span>
+    <ul class="container__form-input-block-list">
+        <li class="container__form-input-block-list-item">Chương trình cử nhân Đa phương tiện 2+2 với Đại học Huddersfield, Vương Quốc Anh</li>
+        <li class="container__form-input-block-list-item">Chương trình cử nhân Công nghệ thông tin 2+2 với Đại học Huddersfield, Vương Quốc Anh</li>
+        <li class="container__form-input-block-list-item">Chương trình cử nhân An toàn thông tin 2+2 với Đại học Huddersfield, Vương Quốc Anh</li>
+        <li class="container__form-input-block-list-item">Chương trình cử nhân Công nghệ kỹ thuật điện, điện tử 2+2 với Đại học Huddersfield, Vương Quốc Anh</li>
+    </ul>
+    <span class="container__form-input-block-del-childBlock">Xóa</span>
+</div>
+<div class="container__form-input-block-invisible">
     
-// </div>
-// <button class="container__form-input-block-add">
-//     <img src="./assets/font/svg/Group 10311.svg" alt="">
-//     THÊM NGÀNH XÉT TUYỂN
-// </button>
-// </div>`;
+</div>
+<button class="container__form-input-block-add">
+    <img src="./assets/font/svg/Group 10311.svg" alt="">
+    THÊM NGÀNH XÉT TUYỂN
+</button>
+</div>`);
 
-// // Mảng chứa các chương trình từ chương trình thứ 2
-// var arr = [program];
+// Mảng chứa các chương trình từ chương trình thứ 2
+var arr = [program.firstChild];
 
-// // Heading của form
-// const heading = `<span class="container__form-name">ĐĂNG KÝ XÉT TUYỂN</span>
-// <div class="container__form-heading">
-//     <span>Thông tin đăng ký</span>
-//     <span>*bắt buộc</span>
-// </div>
-// <span class="container__form-desc">
-//     Sau khi đã đọc và hiểu rõ các quy định về tiêu chí và điều kiện xét tuyển của Nhà trường, tôi đồng ý xét tuyển học bạ vào chương trình Liên kết đào tạo Cử nhân như sau: (chọn phương thức xét tuyển)
-// </span>
-// <div class="container__form-part1-program-add">
-//     <span>CHƯƠNG TRÌNH</span>
-//     <div class="container__form-part1-program-add-img">
-//         <img src="./assets/font/svg/Group 10304de.svg" alt="">
-//         <img src="./assets/font/svg/Group 10304.svg" alt="">
-//     </div>
-// </div>`;
+// Heading của form
+const heading = `<span class="container__form-name">ĐĂNG KÝ XÉT TUYỂN</span>
+<div class="container__form-heading">
+    <span>Thông tin đăng ký</span>
+    <span>*bắt buộc</span>
+</div>
+<span class="container__form-desc">
+    Sau khi đã đọc và hiểu rõ các quy định về tiêu chí và điều kiện xét tuyển của Nhà trường, tôi đồng ý xét tuyển học bạ vào chương trình Liên kết đào tạo Cử nhân như sau: (chọn phương thức xét tuyển)
+</span>
+<div class="container__form-part1-program-add">
+    <span>CHƯƠNG TRÌNH</span>
+    <div class="container__form-part1-program-add-img">
+        <img src="./assets/font/svg/Group 10304de.svg" alt="">
+        <img src="./assets/font/svg/Group 10304.svg" alt="">
+    </div>
+</div>`;
 
-// let addBtn = $('.container__form-part1-program-add-img');
+let addBtn = $('.container__form-part1-program-add-img');
 
-// addBtn.onclick = function() {
-//     arr.push(program);
-//     console.log(arr)
-//     render();
-// }
-
-// function render() {
-//     const form = $('.container__form-input-all');
-//     let htmls = "";
-//     arr.forEach(function(item) {
-//         htmls += item;
-//     })
-//     form.innerHTML = htmls;
-// }
+addBtn.onclick = function() {
+    const form = document.getElementById('chuong-trinh');
+    form.append(program.cloneNode(true).firstChild);
+    inputItems = $$('.container__form-input-block');
+    reconvertButton();
+}
 
 // Xử lí chuyển tab
  const tabs = $$('.container__navbar-content-item');
@@ -92,7 +85,7 @@ isValid = true;
  })
 
 // Xử lý nhập thông tin
-const inputItems = $$('.container__form-input-block');
+function reconvertButton(){
 
 inputItems.forEach(function(inputItem, index) {
     const textInput = inputItem.querySelector('input');
@@ -137,6 +130,9 @@ inputItems.forEach(function(inputItem, index) {
         }
     }
 })
+}
+
+reconvertButton();
 
 // Tổ hợp xét tuyển
 const complex = {
